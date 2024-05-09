dc2600299619d5c46e976fd837820a97f0d3c37a
Martin Donlon <github-martin@donlons.com>
2024-05-09 08:44:31 -0700

    Flip endian

--- util/mif2bin.py
+++ util/mif2bin.py
@@ -8,7 +8,7 @@ with open(sys.argv[1]) as f:
 vals = []
 for m in mem:
     m = list(reversed(m))
-    for i in range(0, len(m), 8):
+    for i in reversed(range(0, len(m), 8)):
         bits = m[i:i+8]
         val = 0
         for b in bits:
